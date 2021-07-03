23f04126789f83e26f6761eccc3bb3da46eb115e
muiceking <scm1400@gmail.com>
2021-07-03 15:29:18 +0900

    이해 안가는 부분 많지만 재밌다

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+
 //import PropTypes from "prop-types";
 
 
@@ -86,12 +86,56 @@ import React from 'react';
 //       </div>
 //   }
 // }
-
-class App extends React.Component{
-  render(){
-    return <div>
-      
-      </div>
+import React from 'react';
+import axios from "axios"; //'npm i axos' 설치
+import Movie from "./Movie";
+import "./App.css"
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");  // sort_by=rating
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    // setTimeout(() => {
+    //   this.setState({isLoading: false});  // setState 에서 state에 있는 항목을 호출할 필요는 없다, 새로 생성해도 됨.
+    // },6000);
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+    <section class="container">
+      {isLoading
+        ? (
+        <div class="loader">
+          <span class="loader__text">Loading...</span>
+        </div>
+        ) : (
+          <div class="movies">
+            {
+              movies.map(movie => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              ))   //map을 사용하면 return이 있어야함
+            }
+          </div>
+        )}
+    </section>
+    );
   }
 }
 
